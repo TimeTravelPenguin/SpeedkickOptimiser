23b431a21a3338639ffd8d1efa319ef7ee5a1381
Phillip <Filuperwinsmith@gmail.com>
2019-08-03 22:29:45 +1000

    Update frm_Main.cs

--- SpeedkickOptimiser/frm_Main.cs
+++ SpeedkickOptimiser/frm_Main.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: SpeedkickOptimiser
+// Project: SpeedkickOptimiser
+// File Name: frm_Main.cs
+// 
+// Current Data:
+// 2019-08-03 10:14 PM
+// 
+// Creation Date:
+// 2019-01-20 11:17 AM
+
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 // By TimeTravelPenguin
@@ -14,8 +26,7 @@ namespace SpeedkickOptimiser
 {
     public partial class frm_Main : Form
     {
-
-        // This application will calculate angles to be used for a "speedkick" technique
+        // This application will calculate angles to be used for a "speed-kick" technique
         // specific to Super Mario 64 Tool-Assisted Speedrunning.
         // It is important to note that this logic works by taking RAW Data, being the analogue
         // stick's X and Y positions, and converting it to a precessed coordinate system used by
@@ -31,9 +42,9 @@ namespace SpeedkickOptimiser
         {
             btn_Calculate.Enabled = false;
 
-            int x = (int)numStickX.Value;
-            int y = (int)numStickY.Value;
-            double deltaAngle = (double)numAngle.Value * (Math.PI / 180); // convert to radians
+            var x = (int) numStickX.Value;
+            var y = (int) numStickY.Value;
+            var deltaAngle = (double) numAngle.Value * (Math.PI / 180); // convert to radians
 
             dataGrid_StickOutput.Rows.Clear();
             Speedkick(x, y, deltaAngle);
@@ -42,62 +53,70 @@ namespace SpeedkickOptimiser
         }
 
         /// <summary>
-        /// Calculates speedkick coordinates
+        ///     Calculates speed-kick coordinates
         /// </summary>
-        /// <param name="rawstickX">The X coordinate on the TAS Tool</param>
-        /// <param name="rawstickY">The Y coordinate on the TAS Tool</param>
-        /// <param name="deltaAngle">The off-axis angle in degrees to check for speedkick coordinates</param>
+        /// <param name="rawStickX">The X coordinate on the TAS Tool</param>
+        /// <param name="rawStickY">The Y coordinate on the TAS Tool</param>
+        /// <param name="deltaAngle">The off-axis angle in degrees to check for speed-kick coordinates</param>
         private void Speedkick(int rawStickX, int rawStickY, double deltaAngle)
         {
             // update labels
-            Controller inputController = new Controller(rawStickX, rawStickY);
+            var inputController = new Controller(rawStickX, rawStickY);
             lbl_Magnitude.Text = $"{inputController.stickMag}";
-            lbl_Angle.Text = $"{(float)(inputController.stickAngle * (180 / Math.PI))}";
+            lbl_Angle.Text = $"{(float) (inputController.stickAngle * (180 / Math.PI))}";
 
-            double angle = inputController.stickAngle;
+            var angle = inputController.stickAngle;
 
-            double maxAngle = angle + deltaAngle;
-            double minAngle = angle - deltaAngle;
+            var maxAngle = angle + deltaAngle;
+            var minAngle = angle - deltaAngle;
 
             // do max coords for new angle
-            double r = 48; // max speedkick magnitude
+            const double r = 48; // max speed-kick magnitude
 
-            int maxX = (int)Math.Ceiling(r * Math.Cos(maxAngle) - 6);
-            int minX = (int)Math.Ceiling(r * Math.Cos(minAngle) + 6);
+            var maxX = (int) Math.Ceiling(r * Math.Cos(maxAngle) - 6);
+            var minX = (int) Math.Ceiling(r * Math.Cos(minAngle) + 6);
 
-            int maxY = (int)Math.Ceiling(r * Math.Sin(maxAngle) + 6);
-            int minY = (int)Math.Ceiling(r * Math.Sin(minAngle) - 6);
+            var maxY = (int) Math.Ceiling(r * Math.Sin(maxAngle) + 6);
+            var minY = (int) Math.Ceiling(r * Math.Sin(minAngle) - 6);
 
             // find max Mag
-            List<Controller> controllerList = new List<Controller>();
+            var controllerList = new List<Controller>();
 
             // the minimum and maximum angle bounds
             var maxTangent = Math.Atan2(maxY, maxX);
             var minTangent = Math.Atan2(minY, minX);
 
             // list all angles in range
-            for (int x = 0; x <= minX; x++)
-                for (int y = 0; y <= maxY; y++)
+            for (var x = 0; x <= minX; x++)
+            {
+                for (var y = 0; y <= maxY; y++)
                 {
                     var theta = Math.Atan2(y, x);
 
-                    if (theta >= minTangent && theta <= maxTangent) // if (x, y) is in regioned angle bounds
+                    if (theta >= minTangent && theta <= maxTangent) // if (x, y) is in angle bounds
                     {
                         var c = new Controller(x, y);
                         if (c.stickMag <= r)
+                        {
                             controllerList.Add(c);
+                        }
                     }
                 }
+            }
 
-            List<Controller> SortedMagList = controllerList.OrderByDescending(o => o.stickMag).ToList();
+            var sortedMagList = controllerList.OrderByDescending(o => o.stickMag);
 
-            // put data in datagrid
-            foreach (var item in SortedMagList)
-                dataGrid_StickOutput.Rows.Add(new object[] { item.rawStickX, item.rawStickY, (float)(item.stickAngle * (180 / Math.PI)), (float)((item.stickAngle - inputController.stickAngle) * (180 / Math.PI)), (float)item.stickMag });
+            // put data in data-grid
+            foreach (var item in sortedMagList)
+            {
+                dataGrid_StickOutput.Rows.Add(item.rawStickX, item.rawStickY,
+                    (float) (item.stickAngle * (180 / Math.PI)),
+                    (float) ((item.stickAngle - inputController.stickAngle) * (180 / Math.PI)), item.stickMag);
+            }
 
-            // sort datagrid
-            dataGrid_StickOutput.Sort(dataGrid_StickOutput.Columns["stickMag"], ListSortDirection.Descending);
-
+            // sort data-grid
+            dataGrid_StickOutput.Sort(dataGrid_StickOutput.Columns["stickMag"] ?? throw new InvalidOperationException(),
+                ListSortDirection.Descending);
         }
 
 
